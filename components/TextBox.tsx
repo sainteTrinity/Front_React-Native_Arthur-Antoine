3ee3716b82cff2725fc972f47ec1fab0d0f10755
antoine <antoinepellegrino.dev@gmail.com>
2023-05-24 14:18:09 +0200

    Correction icon

--- components/TextBox.tsx
+++ components/TextBox.tsx
@@ -23,7 +23,7 @@ const TextBox = (props: TextBoxProps) => {
 
 
             <TextInput left={icon ? <TextInput.Icon icon={icon ? icon : ''}/> : null}
-                       right={secureTextEntry ? <TextInput.Icon icon={'eye'} onPress={() => showPassword()}/> : null} value={content}
+                       right={secureTextEntry ? <TextInput.Icon icon={isPasswordVisible ? 'eye-off' : 'eye'} onPress={() => showPassword()}/> : null} value={content}
                        onChangeText={text => setContent ? setContent(text) : null}
                        style={styles.textInput} underlineColor={"transparent"} placeholder={content ? content : ''}
                        secureTextEntry={secureTextEntry && !isPasswordVisible}/>
