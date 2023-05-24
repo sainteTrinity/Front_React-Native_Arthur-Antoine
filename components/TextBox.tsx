1b9a82354f0a5e91ca259aea658b24d0626b6486
antoine <antoinepellegrino.dev@gmail.com>
2023-05-24 11:07:24 +0200

    Auto stash before checking out "origin/main"

--- components/TextBox.tsx
+++ components/TextBox.tsx
@@ -5,15 +5,23 @@ import {StyleSheet, View} from "react-native";
 type TextBoxProps = {
     content?: string;
     setContent?: (content: string) => string;
+    icon?: string;
+    secureTextEntry?: boolean;
 
 }
 const TextBox = (props: TextBoxProps) => {
-    const {content, setContent} = props;
+    const {content, setContent, icon, secureTextEntry} = props;
 
     return (
         <View style={styles.textBox}>
-            <TextInput value={content} onChangeText={text => setContent ? setContent(text) : null}
-                       style={styles.textInput} underlineColor={"transparent"}/>
+
+
+            <TextInput left={icon ? <TextInput.Icon icon={icon ? icon : ''}/> : null}
+                       right={secureTextEntry ? <TextInput.Icon icon={'eye'}/> : null} value={content}
+                       onChangeText={text => setContent ? setContent(text) : null}
+                       style={styles.textInput} underlineColor={"transparent"} placeholder={content ? content : ''}
+                       secureTextEntry={secureTextEntry}/>
+
         </View>
     )
 
