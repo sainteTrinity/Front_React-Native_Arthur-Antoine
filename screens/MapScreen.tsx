4619e13fc3e42c534cec1667d77b6d86d864610e
Antoine <antoinepellegrino.dev@gmail.com>
2023-06-18 17:49:54 +0200

    Add buton locate

--- screens/MapScreen.tsx
+++ screens/MapScreen.tsx
@@ -1,8 +1,8 @@
 import React, {useEffect, useState} from "react";
-import {View} from "react-native";
 import MapView, {UrlTile} from "react-native-maps";
 import * as Location from 'expo-location';
 import {LocationObject} from "expo-location";
+import { View, Button, TouchableOpacity } from "react-native";
 
 
 const MapScreen = () => {
@@ -23,21 +23,36 @@ const MapScreen = () => {
     }, []);
 
 
+
+
     return (
-            <MapView
+            <>
+                <MapView
 
-                style={{ flex: 1 }}
-                region={{
-                    latitude: location?.coords.latitude || 0,
-                    longitude: location?.coords.longitude || 0,
-                    latitudeDelta: 0.0922,
-                    longitudeDelta: 0.0421,
-                }
-                }
-            >
-                <UrlTile urlTemplate={"http://c.tile.openstreetmap.org/{z}/{x}/{y}.png"} />
-            </MapView>
+                    style={{ flex: 1 }}
+                    region={{
+                        latitude: location?.coords.latitude || 0,
+                        longitude: location?.coords.longitude || 0,
+                        latitudeDelta: 0.0922,
+                        longitudeDelta: 0.0421,
+                    }
+                    }
+                >
+                    <UrlTile urlTemplate={"http://c.tile.openstreetmap.org/{z}/{x}/{y}.png"} />
+                </MapView>
+                <TouchableOpacity
+                    style={{
+                        position: 'absolute',
+                        bottom: 16,
+                        right: 16,
+                        backgroundColor: 'white',
+                        borderRadius: 32,
+                        padding: 8,
+                    }}
+                >
 
+                </TouchableOpacity>
+            </>
 
     )
 }
